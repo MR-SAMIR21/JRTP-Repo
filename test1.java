fcb6a8de25d36641481c100d3fb1cd0fe33936e7
MR.SAMIR <samirkumarpanjha@gmail.com>
2025-07-07 12:55:55 +0530

    task 123 completed

--- test1.java
+++ test1.java
@@ -15,5 +15,11 @@ public class test1{
       int y=x*x;
       System.out.println("square ::"+y);
       System.out.println("m7 completed task456");
+
+   public void m6(){
+      int x=10;
+      int y=x*x*x;
+      System.out.println("cube ::"+y);
+      System.out.println("task123 is going on");
    }
 }
